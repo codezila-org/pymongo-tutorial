27ffa4f6436480db4719058032bfe3e305087a99
AmanSingour <amansingour15@gmail.com>
2020-04-24 22:47:38 +0530

    CODEZILA | pymongo

--- createDB.py
+++ createDB.py
@@ -6,10 +6,10 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://<yourusername>:<yourpassword>@cluster0-mgbud.mongodb.net/test?retryWrites=true&w=majority")
 
 # cREATING dATABASE... 
-db = client['grey-market']
+db = client['DbName']
 
 # cREATING cOLLECTION...
-usersCollection = db['Users']
+usersCollection = db['collName']
 
 data = { "name": "User Name", "email": "useremail@domain.com", "age": 20 }
 
